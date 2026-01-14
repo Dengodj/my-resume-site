9d681ef806fc6827172e15fe6546234babbf5162
Denis Goida <www.vdcdj@gmail.com>
2026-01-14 22:25:47 +0200

    Deploy: update production build with new responsive styles

--- docs/chunk-TCKYH4UV.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-GOS4GYSI.js";import"./chunk-7UXJVFCN.js";import"./chunk-OV4SVJWI.js";import"./chunk-RX3EWOSR.js";import"./chunk-Q7JTLJGB.js";export{a as Skills};
---
+++ docs/chunk-XT3O3BCQ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-TJAWZ6KJ.js";import"./chunk-7UXJVFCN.js";import"./chunk-OV4SVJWI.js";import"./chunk-RX3EWOSR.js";import"./chunk-Q7JTLJGB.js";export{a as Skills};
